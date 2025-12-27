41cf0bf9140355d12d8bc919642afefb77aa9ea7
Chris <davischris2207@gmail.com>
2025-12-27 14:24:17 +0000

    Pressure Plate Constructor

--- Source/MultiCoOpAdventure/PressurePlate.cpp
+++ Source/MultiCoOpAdventure/PressurePlate.cpp
@@ -9,12 +9,48 @@ APressurePlate::APressurePlate()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	bReplicates = true;
+	SetReplicateMovement(true);
+
+	Activated = false;
+
+	RootComp = CreateDefaultSubobject<USceneComponent>(TEXT("RootComp"));
+	SetRootComponent(RootComp);
+
+	TriggerMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TriggerMesh"));
+	TriggerMesh->SetupAttachment(RootComp);
+	TriggerMesh->SetIsReplicated(true);
+
+	auto TriggerMeshAsset = ConstructorHelpers::FObjectFinder<UStaticMesh>(
+		TEXT("/Game/LevelPrototyping/Meshes/SM_Cylinder.SM_Cylinder"));
+	if (TriggerMeshAsset.Succeeded())
+	{
+		TriggerMesh->SetStaticMesh(TriggerMeshAsset.Object);
+		TriggerMesh->SetRelativeScale3D(FVector(3.3f, 3.3f, 0.2f));
+		TriggerMesh->SetRelativeLocation(FVector(0.0f, 0.0f, 10.0f));
+	}
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(RootComp);
+	Mesh->SetIsReplicated(true);
+
+	auto MeshAsset = ConstructorHelpers::FObjectFinder<UStaticMesh>(
+		TEXT("/Game/Stylized_Egypt/Meshes/building/SM_building_part_08.SM_building_part_08"));
+	if (MeshAsset.Succeeded())
+	{
+		Mesh->SetStaticMesh(MeshAsset.Object);
+		Mesh->SetRelativeScale3D(FVector(4.0f, 4.0f, 0.5f));
+		Mesh->SetRelativeLocation(FVector(0.0f, 0.0f, 7.2f));
+	}
 }
 
 // Called when the game starts or when spawned
 void APressurePlate::BeginPlay()
 {
 	Super::BeginPlay();
+
+	TriggerMesh->SetVisibility(false);
+	TriggerMesh->SetCollisionProfileName(FName("OverlapAll"));
 	
 }
 
--- Source/MultiCoOpAdventure/PressurePlate.h
+++ Source/MultiCoOpAdventure/PressurePlate.h
@@ -4,7 +4,6 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "Component/StaticMeshComponent.h"
 #include "PressurePlate.generated.h"
 
 UCLASS()
